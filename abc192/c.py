8c477428735253117b8d3521aaa64975f3833df6
Willco21 <zyapanetto@gmail.com>
2021-02-27 14:19:31 +0900

    fix

--- abc192/c.py
+++ abc192/c.py
@@ -1,4 +1,4 @@
-def kaprekar_function(n, digits):
+def kaprekar_function(n, digits, is_reverse):
     l =  [int(x) for x in list(str(n).zfill(digits))]
     l.sort(reverse=is_reverse)
     return l
@@ -6,15 +6,16 @@ def kaprekar_function(n, digits):
 def kaprekar_routine(n, is_reverse):
     pre = n
     digits = len(str(n))
-    suc = kaprekar_function(n, digits=digits)
+    suc = kaprekar_function(n, digits=digits, is_reverse=is_reverse)
     print(suc)
+    return suc
 
 N, K = map(int, input().split())
 
 a = [N]
 
 for i in range(K):
-    a[i+1] = kaprekar_routine(a[i],True)-kaprekar_routine(a[i],False)
+    a.append(kaprekar_routine(a[i],True) - kaprekar_routine(a[i],False))
 
 
 print(a[K])
